b32b10b7ad0a80287215693f72e71a487bac0650
Your Name <you@example.com>
2020-09-16 17:28:57 +0800

    del all file

--- JavaBaseProject/src/ibm/com/exception/ThrowsExceptionTest.java
+++
@@ -1,9 +0,0 @@
-package ibm.com.exception;
-public class ThrowsExceptionTest {
-   public void test() throws RuntimeException{
-     throw new RuntimeException();
-   }	     
-   public static void main(String[] args) {
-	   new ThrowsExceptionTest().test();
-   }
-}
